227d46730e2a8cd77980e2bb988847ae0ba72e05
Shaun <32381182+ShaunJae@users.noreply.github.com>
2025-12-19 12:17:41 -0500

    Getting data straight from sheet
    Gets the run count and today numbers from the "100k Sheet" tab in the spreadsheet

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 /*
 
-Looks at the past <lookbackCount> runs and counts how many before a gap of <inactivePeriod>
- occurs between two consecutive runs date_played_est_2 times. Reads the 100k sheet to do so
-
-Run times (date_played_est_2) is in column CJ
-Run number (run_id) is in column CO
+Pulls data from google sheets and formats onto a browser page 
 
 */
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
 
 var sheetsKey = urlParams.get("key")
 const spreadsheetID = "1Tyw9fwdZgsHJoHzlE-0LPSEDOduRkZwL2UUNA-_4Xo4"
@@ -16,19 +12,16 @@ const spreadsheetID = "1Tyw9fwdZgsHJoHzlE-0LPSEDOduRkZwL2UUNA-_4Xo4"
 var totalElement = document.getElementById("total")
 var todayElement = document.getElementById("today")
 
+var updateRate = urlParams.get("update_rate") == null ? 10 : parseInt(urlParams.get("update_rate"))
 
-var inactivePeriod = urlParams.get("inactivity_period") == null ? 8 : parseInt(urlParams.get("inactivity_period"))
-var lookbackCount = urlParams.get("lookback_count") == null ? 40 : parseInt(urlParams.get("lookback_count"))
-
-var inactivePeriodMs = inactivePeriod * 60 * 60 * 1000
 var totalRunId = ""
 
 // Opens http request
 function httpGet(url){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url + "?key=" + sheetsKey, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.open("GET", url + "?key=" + sheetsKey, false)
+    xmlHttp.send(null)
+    return xmlHttp.responseText
 }
 
 /**
@@ -44,47 +37,18 @@ function getSheetData(sheet, cellRange){
 }
 
 /**
- * @param {number} count        How many date_played_est_2 to return as an int 
- * @returns 2D array of length 'count' with the column date_played_est_2
- */
-function getRunTimes(count){
-    return getSheetData("Raw Data", "CJ4:CJ" + (4 + count - 1).toString())
-}
-
-/**
  * @returns {number} An integer of current run number
  */
 function getTotalRunID(){
-    var response = getSheetData("Raw Data", "CO4:CO4")
+    var response = getSheetData("100k Stats", "B5:B5")
     return parseInt(response[0][0].replaceAll(",",""))
 }
 
 /**
- * @returns The most recent runs date_played_est_2 
- */
-function getLastRunTime(){
-    return JSON.parse(getRunTimes(1))["values"][0][0]
-}
-
-/**
- * @returns The number of runs completed within lookbackCount runs without a gap of inactivePeriod hours
+ * @returns The number of runs completed from the 100k sheet
  */
 function getTodaysRuns(){
-    var lookbackRuns = getRunTimes(lookbackCount)
-
-    if(new Date() - new Date(lookbackRuns[0][0]) > inactivePeriodMs){
-        return 0
-    }
-
-    var runsToday = 1
-
-    for(var i = 0; i < lookbackRuns.length - 1; i++){
-        if(new Date(lookbackRuns[i][0]) - new Date(lookbackRuns[i+1][0]) > inactivePeriodMs){
-            break
-        }
-        runsToday++
-    }
-    return runsToday
+    return getSheetData("100k Stats", "E6:E6") 
 }
 
 /**
@@ -105,5 +69,4 @@ updateCounter()
 //Checks ever 10 seconds if a new run is on the sheet. Updates counter if so
 window.setInterval(function(){
     updateCounter()
-}, 10000)
-
+}, updateRate * 1000)
